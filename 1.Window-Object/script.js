1948bc6ed44c7db35e3541e16e70b2829d5edd8f
Manas Ranjan Bera <manasbera484@gmail.com>
2024-12-13 15:20:36 +0530

    first commit

---
+++ 1.Window-Object/script.js
@@ -0,0 +1,21 @@
+//!Window Object
+// console.log(window);
+// console.log(window.document);
+// console.log(window.document.body);
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+// console.log(window.location);
+// console.log(window.location.href);
+// console.log(window.history);
+// console.log(window.history.length);
+// console.log(window.navigator);
+// console.log(window.navigator.userAgent);
+// console.log(window.alert("Hello World"));
+// console.log(window.confirm("Are you sure?"));
+// console.log(window.screen);
+// window.alert("Hello World");
+// window.open("https://www.google.com", "_blank");
+
+alert("Hello World");
+confirm("Are you sure?");
+prompt("Enter your name");
