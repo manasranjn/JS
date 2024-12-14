65f054b197a38b6699fb548d33e724587db8ce2a
Manas Ranjan Bera <manasbera484@gmail.com>
2024-12-14 08:06:58 +0530

    querySelector

---
+++ 4.querySelector/script.js
@@ -0,0 +1,15 @@
+//Get the first element with the class "product-card"
+const FirstProductEl = document.querySelector(".product-card")
+console.log(FirstProductEl);
+
+//Get the highest rated product
+const highestRatingProductEl = document.querySelector("[data-rating='4.8']")
+console.log(highestRatingProductEl);
+
+//To get the first laptop product
+const firstLaptopEl = document.querySelector("[data-category='laptop']")
+console.log(firstLaptopEl);
+
+//To select the maon container
+const parentEl = document.querySelector("#dashboard")
+console.log(parentEl)
