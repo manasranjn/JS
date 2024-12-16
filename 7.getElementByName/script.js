256e752a81dd5c104e28b6a1e90a5b649af1f96f
Manas Ranjan Bera <manasbera484@gmail.com>
2024-12-16 15:27:20 +0530

    getElementsByName

---
+++ 7.getElementByName/script.js
@@ -0,0 +1,5 @@
+const usernameEl = document.getElementsByName("username")
+// console.log(usernameEl);
+
+const languageEl = document.getElementsByName("languageProficiency")
+// console.log(languageEl); 
