08579510b02b45eab05beebfd38c8ef070011e94
Manas Ranjan Bera <manasbera484@gmail.com>
2024-12-30 20:11:57 +0530

    getElementByName

--- 7.getElementByName/script.js
+++ 7.getElementByName/script.js
@@ -1,5 +1,18 @@
-const usernameEl = document.getElementsByName("username")
+const usernameEl = document.getElementsByName("username");
 // console.log(usernameEl);
 
-const languageEl = document.getElementsByName("languageProficiency")
-// console.log(languageEl); 
+const languageEl = document.getElementsByName("languageProficiency");
+// console.log(languageEl);
+
+const emailEl = document.getElementsByName("email");
+// console.log(emailEl);
+
+function collectData() {
+  const languageEl = document.getElementsByName("languageProficiency");
+
+  for (let i = 0; i < languageEl.length; i++) {
+    if (languageEl[i].checked) {
+      console.log(languageEl[i].value);
+    }
+  }
+}
